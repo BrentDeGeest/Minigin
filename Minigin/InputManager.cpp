3914431ca5d13f8ae0d2e7aa11982ca693c06135
Brent <brentdegeest@live.be>
2024-03-18 13:08:17 +0100

    removed examples by imgui

--- Minigin/InputManager.cpp
+++ Minigin/InputManager.cpp
@@ -1,16 +1,21 @@
 #include <SDL.h>
 #include "InputManager.h"
 #include <backends/imgui_impl_sdl2.h>
+#include <iostream>
+///////////////////
+// slide 13 xinput
+///////////////////
 
 bool dae::InputManager::ProcessInput()
 {
+
 	SDL_Event e;
 	while (SDL_PollEvent(&e)) {
 		if (e.type == SDL_QUIT) {
 			return false;
 		}
 		if (e.type == SDL_KEYDOWN) {
-			
+			std::cout << "key pressed!\n";
 		}
 		if (e.type == SDL_MOUSEBUTTONDOWN) {
 			
